32cc6b1735708a74783b5e5883bac6d45b6d2a17
MustafaKyl <mustafa_villager@hotmail.com>
2024-12-02 11:24:36 +0300

    Update drugCondition.py

--- drugCondition.py
+++ drugCondition.py
@@ -8,13 +8,10 @@ from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
 import json
 credentials_dict = json.loads(st.secrets["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
-st.write(type(credentials_dict))
-st.write(credentials_dict)
 
 credentials = Credentials.from_service_account_info(credentials_dict)
 
-st.write(type(credentials))
-st.write(credentials)
+
 
 client = vision.ImageAnnotatorClient(credentials=credentials)
 
@@ -22,6 +19,8 @@ client = vision.ImageAnnotatorClient(credentials=credentials)
 pd.set_option("display.max_columns",1000)
 pd.set_option("display.width",500)
 pd.set_option("display.max_rows",50)
+
+#data preprocessing
 #df = pd.read_csv("drug_information.csv")
 #df.dropna(inplace=True)
 #df.isnull().sum()
@@ -73,17 +72,16 @@ if downloadImage is not None:
         start_point = soup.find(id="uses")
         end_point = soup.find(class_="ddc-related-link-group")
 
-        # Başlangıç noktasından sonraki tüm kardeş öğeleri al
         content = []
         for sibling in start_point.find_next_siblings():
             if sibling == end_point:
-                break  # Bitiş noktasına geldiğinde döngüyü sonlandır
-             #Metin yerine ham HTML'yi al
+                break 
+             
 
             for a_tag in sibling.find_all('a'):
                 a_tag.unwrap()
             content.append(str(sibling))
-                # Sonuçları birleştir
+                
         result = "\n".join(content)
         return result
 
@@ -91,6 +89,7 @@ if downloadImage is not None:
 
     drug_name = df.loc[df["drugName"].apply(lambda x:x.lower() in text.lower()),"drugName"].iloc[0]
     side_effects = get_side_effects(drug_name)
+    
     #5000 karakter sınırı olduğu için 2 parçaya bölüp öyle çeviriyorum
     translated_content = GoogleTranslator(source='en', target='tr').translate(side_effects[:4000])
     translated_content2 = GoogleTranslator(source="en", target="tr").translate(side_effects[4000:])
